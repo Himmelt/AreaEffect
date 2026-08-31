570e602a8f530a9c38e7ab12f541fb2d4b2bfec7
Himmelt <master@void-3.cn>
2026-08-31 16:32:15 +0800

    Rename mod LightArea -> AreaEffect (modId: areaeffect)

---
+++ src/main/java/org/soraworld/areaeffect/handler/EventBusClientHandler.java
@@ -0,0 +1,37 @@
+package org.soraworld.areaeffect.handler;
+
+import net.minecraft.client.gui.GuiVideoSettings;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.soraworld.areaeffect.proxy.ClientProxy;
+import org.soraworld.areaeffect.proxy.CommonProxy;
+
+/**
+ * @author Himmelt
+ */
+public class EventBusClientHandler {
+
+    private final ClientProxy proxy;
+
+    public EventBusClientHandler(ClientProxy proxy) {
+        this.proxy = proxy;
+    }
+
+    @SubscribeEvent
+    public void onVideoSetting_new(GuiScreenEvent.DrawScreenEvent.Post event) {
+        if (CommonProxy.v_1_8) {
+            if (event.gui instanceof GuiVideoSettings) {
+                proxy.saveLight();
+            }
+        } else if (event.getGui() instanceof GuiVideoSettings) {
+            proxy.saveLight();
+        }
+    }
+
+    @cpw.mods.fml.common.eventhandler.SubscribeEvent
+    public void onVideoSetting_old(GuiScreenEvent.DrawScreenEvent.Post event) {
+        if (event.gui instanceof GuiVideoSettings) {
+            proxy.saveLight();
+        }
+    }
+}
